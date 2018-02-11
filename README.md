1175e20ec9ee54499caf813516bce8cd361305e0
Stephen Slatky <steve.slatky@gmail.com>
2018-02-11 01:32:34 -0500

    Gets paths of images - right now jpg only

---
+++ README.md
@@ -0,0 +1,23 @@
+# Organize Photos
+**Author:** Stephe Slatky  
+**Date:** 2018-02-10
+
+# What does it do? 
+
+Give it a folder and it will organize photos to your picture library. 
+
+## Structure
+
+# Todo 
+
+* Have initial project plan
+
+* I want to get the folder with photos.
+
+* Get date from images
+
+* Rename images to: yyyy-mm-dd-#
+
+* Store images in folder structure: yyyy -> month
+
+* Handle either Exif or XMP data
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate rexiv2;
+extern crate glob;
+use glob::glob;
+
 fn main() {
-    println!("Hello, world!");
+
+    let folder = "./test_data/";
+
+    let tag_time = "Exif.Image.DateTimeOriginal";
+    //let meta = rexiv2::Metadata::new_from_path("./test_data/IMG_0754.jpg").unwrap();
+    //let test =  meta.get_xmp_tags().expect("can't get tages");
+    //println!("{:?}", meta.get_exif_tags());
+
+    get_images(folder);
 }
+
+fn get_images(path:&str) -> Vec< std::path::PathBuf > {
+    let mut imgs = Vec::new();
+    for entry in glob(&(path.to_owned() + "*.jpg")).expect("Can not find directory.") {
+        match entry {
+            Ok(p) => imgs.push(p),
+            // if the path matched but was unreadable,
+            // thereby preventing its contents from matching
+            Err(e) => println!("{:?}", e),
+        }
+    }
+    imgs
+}
+
